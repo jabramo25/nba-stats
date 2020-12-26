3334a725c4bb0efbb3a597eed15abf764a3afa7f
Jessica Abramovich <jla1501@gmail.com>
2020-12-26 16:16:32 -0500

    Add server with sequelize/mysql/express

---
+++ app/controllers/player.controller.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+const Player = db.players;
+const Op = db.Sequelize.Op;
+
+// Retrieve all Player from the database.
+exports.findAll = (req, res) => {
+    const title = req.query.name;
+    var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
+
+    Player.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving Player."
+            });
+        });
+};
+
+// Find a single Player with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Player.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Player with id=" + id
+            });
+        });
+};
---
+++ app/models/player.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+    const Player = sequelize.define("player", {
+        name: {
+            type: Sequelize.STRING
+        },
+        year_start: {
+            type: Sequelize.INTEGER
+        },
+        year_end: {
+            type: Sequelize.INTEGER
+        },
+        position: {
+            type: Sequelize.STRING
+        },
+        height: {
+            type: Sequelize.STRING
+        },
+        weight: {
+            type: Sequelize.INTEGER
+        },
+        birth_date: {
+            type: Sequelize.STRING
+        },
+        college: {
+            type: Sequelize.STRING
+        }
+    });
+
+    return Player;
+};
